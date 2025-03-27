0a95980f59d3561fd3c2585913253fb11588398f
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-03-27 17:47:24 +0100

    Scroll work on

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+//const dd = document.querySelector('#dd');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
+       // dd.classList.add('hidden');
     }else{
-        menu.classList.add('hidden');   
+        menu.classList.add('hidden');
+       // dd.classList.remove('hidden');   
      }
+
 })
 
